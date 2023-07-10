b6e2f84ba2e462b461fb81012f5e0d448790269b
Renato Ayau <renato.ayau@gmail.com>
2023-07-10 02:42:37 -0600

    feat: add alias general

--- alias/general.zsh
+++ alias/general.zsh
@@ -1,15 +1,19 @@
 alias ls='lsd'
 alias l='ls'
-alias la='ls -lh'
-alias ll='ls -lh'
-alias lsa='ls -lah'
+alias ll='ls -lhF'
+alias la='ls -AlhF'
 
 alias c='clear'
+alias h='history'
+alias e='exit'
+alias grep='grep --color=auto'
+alias wget='wget -c'
 
 alias mv='mv -i'
 alias cp='cp -i'
 alias ln='ln -i'
 
-alias df='df -h'
+alias df='df -Th --total | sort -h -r | column -t'
 
-alias sysll="systemctl list-unit-files | grep enabled"
+alias sysll="systemctl list-unit-files"
+alias syslls="systemctl list-unit-files --type=service"
